7e51cf474e8b4eee30d176aae9c68feafbd2b6a0
Aret Mimaryan <amimaryan@gmail.com>
2021-05-29 16:47:27 -0400

    Commit project

---
+++ ChatApplication/Models/Message.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ChatApplication.Models
+{
+    public class Message
+    {
+        public int Id { get; set; }
+        
+        [Required]
+        public string UserName { get; set; }
+
+        [Required]
+        public string Text { get; set; }
+
+        public DateTime When { get; set; }
+
+
+    }
+}
